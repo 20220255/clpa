7fd4dbb007fb323098a074e5f279b30726b27e21
20220255 <118017744+20220255@users.noreply.github.com>
2024-11-30 01:26:23 +0800

    1. ClaimFreeWashPageProps

--- app/claimFreeWash/[refId]/page.tsx
+++ app/claimFreeWash/[refId]/page.tsx
@@ -1,8 +1,12 @@
 
+import { PageProps } from "@/.next/types/app/page";
 import ClaimFreeWash from "./ClaimFreeWash"
 
+interface ClaimFreeWashPageProps extends PageProps {
+  params: Awaited<PageProps['params']>;
+}
 
-const ClaimFreeWashPage = async ({ params }: { params: { refId: string } }): Promise<JSX.Element> => {
+const ClaimFreeWashPage = async ({ params }: ClaimFreeWashPageProps): Promise<JSX.Element> => {
 
   const refIdName = await params
   const refId = refIdName.refId
