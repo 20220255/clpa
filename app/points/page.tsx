7d7df4abf916c67a9478e9e2b02a4a4a03c804c4
20220255 <118017744+20220255@users.noreply.github.com>
2024-11-30 18:20:44 +0800

    1. Added logic for getting points in the Customer Points Animation page

--- app/points/page.tsx
+++ app/points/page.tsx
@@ -4,17 +4,35 @@ import Card from "@/components/shared/card/Card"
 import PointsCircles from "./PointsCircles"
 import { Button } from "@/components/ui/button"
 import JSConfetti from "js-confetti"
-import { Suspense, useRef } from "react"
+import { Suspense, useEffect, useRef, useState } from "react"
 import animatePoints from "./utils/animatePoints"
-import { getUserLatestRefPoints } from "@/utils/actions"
+import { getFirstName, getUserLatestRefPoints } from "@/utils/actions"
 import { toast } from "react-toastify"
+import { useAuth } from "@clerk/nextjs"
 
 
 const PointsPage = () => {
 
+    // Get the first name of the logged in user
+    const { userId } = useAuth();
+
   const maxPoints = 8
   const canvasRef = useRef();
   const confettiRef = useRef<JSConfetti | null>(null);
+
+  const [firstName, setFirstName] = useState<string | null>(null)
+  const [pointsLeft, setPointsLeft] = useState<number | null>(null)
+  const [initialRender, setinitialRender] = useState(true);
+
+  useEffect(() => {
+    const getUserName = async () => {
+      if (userId) {
+        const { firstName } = await getFirstName(userId);
+        setFirstName(firstName || '');
+      }
+    }
+    getUserName();
+  }, []);
 
   const handlePointsClaimed = async (points: number): Promise<void> => {
     // limit points to 8
@@ -36,29 +54,53 @@ const PointsPage = () => {
   };
 
   const handleClick = async () => {
-
     const { points, error } = await getUserLatestRefPoints()
-    
     if (error) {
       toast.error(error)
       return
     }
-
+    setPointsLeft(maxPoints - (points ?? 0))
+    setinitialRender(false)
     await handlePointsClaimed(points ?? 0);
   }
+
+  const checkPointsText = (
+    <h5 className="dark:text-gray-100">
+      Hi {firstName}, Click or tap the
+      <span style={{ color: "red" }}> Check</span> button below to show your
+      points.
+    </h5>
+  );
+
+  const completedText = (
+    <h5 className="dark:text-gray-100">
+      Congratulations, {firstName}! You may claim your next wash for
+      free.
+    </h5>
+  );
+
+  const uncompletedText = (
+    <h5 className="dark:text-gray-100">
+      Hi {firstName}. You are <span>{String(pointsLeft)}</span>
+      {pointsLeft && pointsLeft < 2 ? <span> point</span> : <span> points</span>} away from
+      getting your free wash.
+    </h5>
+  );
 
   return (
     <div >
       <Card >
         <div className="flex flex-col items-center">
-          <h5 className="dark:text-gray-100">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h5>
+          {initialRender 
+            ? checkPointsText 
+            : pointsLeft === 0 
+            ? completedText 
+            : uncompletedText}   
           <Suspense fallback={<div>Loading...</div>}>
             <PointsCircles maxPoints={8} />
           </Suspense>
           <Button variant="default" size='lg' className=" bg-blue-300  text-blue-900 flex justify-center items-center relative min-w-full mt-5 hover:bg-blue-400 " onClick={handleClick}>
-            Points
+            Check
           </Button>
         </div>
       </Card>
