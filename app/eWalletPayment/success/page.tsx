fdba278383682b2cd7b7f849dcd0772b12613e65
20220255 <118017744+20220255@users.noreply.github.com>
2024-12-30 17:37:45 +0800

    1. Uncomment Success page component

--- app/eWalletPayment/success/page.tsx
+++ app/eWalletPayment/success/page.tsx
@@ -1,18 +1,18 @@
 // Ewallet on hold
-// 'use client'
+'use client'
 
-// import { Suspense } from "react";
-// // import SuccessPage from "./SuccessPage";
-// import Spinner from "@/components/shared/Spinner";
+import { Suspense } from "react";
+// import SuccessPage from "./SuccessPage";
+import Spinner from "@/components/shared/Spinner";
 
-// const PaymentSuccessPage = () => {
-//   return (
-//     <div>
-//       <Suspense fallback={<Spinner />}>
-//         {/* <SuccessPage /> */}
-//       </Suspense>
-//     </div>
-//   )
-// }
+const PaymentSuccessPage = () => {
+  return (
+    <div>
+      {/* <Suspense fallback={<Spinner />}>
+        <SuccessPage />
+      </Suspense> */}
+    </div>
+  )
+}
 
-// export default PaymentSuccessPage
+export default PaymentSuccessPage
