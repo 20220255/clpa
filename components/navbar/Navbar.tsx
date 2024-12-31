329f3965ba7ea0bab835afa74c2c130fb4c48f81
20220255 <118017744+20220255@users.noreply.github.com>
2024-12-31 13:49:39 +0800

    1. Added Suspense on Navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,15 +3,19 @@ import Container from "../../global/Container"
 import Logo from "./Logo"
 import Profile from "./Profile"
 import RegisteredCustomers from "./RegisteredCustomers";
+import { Suspense } from "react";
+import Spinner from "../shared/Spinner";
 
 const Navbar = async () => {
     const { isRoleAdmin, error } = await isAdmin();
-        return (
+    return (
         <nav className="border-b" >
             <Container className="flex flex-row sm:place-content-between gap-5 justify-between py-3">
                 <Logo isAdmin={isRoleAdmin} error={error} />
-                {isRoleAdmin ? <RegisteredCustomers /> : null}   
-                <Profile />
+                <Suspense fallback={<Spinner />}>
+                    {isRoleAdmin ? <RegisteredCustomers /> : null}
+                    <Profile />
+                </Suspense>
             </Container>
         </nav>
     )
