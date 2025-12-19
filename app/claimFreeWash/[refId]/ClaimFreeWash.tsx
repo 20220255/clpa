b0897ba0189556ffa4444777444a7e636f20a440
20220255 <118017744+20220255@users.noreply.github.com>
2025-12-20 05:41:42 +0800

    1. Downgraded reacr and react-dom to make it compatible with mui and clerk. 2. fixed revalidatePath since it used for server-side actions 3. removed mongopay apis

--- app/claimFreeWash/[refId]/ClaimFreeWash.tsx
+++ app/claimFreeWash/[refId]/ClaimFreeWash.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { AddRefId, getClerkId, updateClaimed } from "@/utils/actions"
-import { revalidatePath } from "next/cache"
+import { refreshPoints } from "@/app/actions/revalidate"
+// import { revalidatePath } from "next/cache"
 import { useRouter } from "next/navigation"
 // import { useRouter } from "next/router"
 import { useEffect } from "react"
@@ -19,7 +20,8 @@ const ClaimFreeWash = ({ refId }: { refId: string }): JSX.Element => {
             const { error } = await updateClaimed(refId, claimedDate)
             if (error) {
                 toast.error(error)
-                revalidatePath("/points")
+                await refreshPoints()
+                // revalidatePath("/points")
             }
 
             const clerkId = (await getClerkId(refId))?.clerkId ?? ''
@@ -27,7 +29,8 @@ const ClaimFreeWash = ({ refId }: { refId: string }): JSX.Element => {
             const { addRefError } = await AddRefId(clerkId)
             if (addRefError) {
                 toast.error(addRefError)
-                revalidatePath("/points")
+                refreshPoints
+                // revalidatePath("/points")
                 return
             }
             toast.success('Free Wash Claimed!')
